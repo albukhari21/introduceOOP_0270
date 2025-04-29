469b86ec769ac35f1594681250fc0ee32a49d536
albukhari21 <albukhariinsankamil@gmail.com>
2025-04-29 10:52:08 +0700

    menambahkan properties Albukhari

--- introduction.cpp
+++ introduction.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
 using namespace std;
 
-    class mahasiswa{
-        public:
-        int nim;
-        string nama;
-        float nilai;
+class mahasiswa{
+public:
+int nim;
+string nama;
+float nilai;
 
-        void print_date(){
-            cout << "NIM = " << nim << endl;
-            cout << "Nama = " << nama << endl;
-            cout << "Nilai = " << nim << endl;
-        }
+void print_data(){
+    cout << "NIM = " << nim << endl;
+    cout << "Nama = " << nama << endl;
+    cout << "Nilai = " << nim << endl;
+}
 
-    };
+};
   
 
-int main() {
+int main(){
     mahasiswa mhs;
     mhs.nim = 2024;
     mhs.nama = "Al bukhari";
     mhs.nilai = 100
 
-    
-    
+    mhs.print_data();
+  
 }
