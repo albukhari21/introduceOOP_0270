151c2e4a84598bde39b2123c72e4dd8e14276378
albukhari21 <albukhariinsankamil@gmail.com>
2025-04-29 10:41:56 +0700

    menambahkan akses modifikasi

---
+++ introduction.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+    class mahasiswa{
+        public:
+        int nim;
+        string nama;
+        float nilai;
+
+    };
+  
+
+int main() {
+    
+}
